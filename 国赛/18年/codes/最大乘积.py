cd9ca4f8024aa5e490e759d108fde027396fac8c
‘shine_hale’ <3116227229@qq.com>
2022-06-13 16:54:46 +0800

    finish 18 year

---
+++ 国赛/18年/codes/最大乘积.py
@@ -0,0 +1,25 @@
+import math
+import itertools
+
+
+def rule(x):
+    for i in range(1, 10):
+        if x.count(str(i)) != 1:
+            return False
+    return True
+
+
+MAX = 0
+x = list(itertools.permutations(range(1, 10), 9 ) )
+for each in x:
+    number = ''.join(list(map(str, each)))
+    for j in range(1, 8):
+        a = int(number[:j])
+        b = int(number[j:])
+        c = str(a * b)
+        if len(c) == 9:
+            if rule(c):
+                if int(c) > MAX:
+                    MAX = int(c)
+print(MAX)
+# print(839542176)
